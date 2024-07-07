5836efe025e48796a1da74bd65985a6a641fd957
rabieHs <rabiehoussaini@gmail.com>
2024-07-07 10:10:00 +0100

    Initial Commit

---
+++ MessageMe/MessageMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMeApp.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 7/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessageMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
