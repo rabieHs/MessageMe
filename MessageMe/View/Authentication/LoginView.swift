9d3658a03106fb6f894de81978cc772db764b157
rabieHs <rabiehoussaini@gmail.com>
2024-07-07 10:45:50 +0100

    initial

---
+++ MessageMe/View/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 7/7/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ MessageMe/View/Authentication/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 7/7/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
