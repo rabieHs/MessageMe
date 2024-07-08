b753571544cee7cb4f8b328908a9c1744c8ab4c2
rabieHs <rabiehoussaini@gmail.com>
2024-07-08 12:47:47 +0100

    settings

---
+++ MessageMe/View/Settings/SettingsHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsHeaderView.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 8/7/2024.
+//
+
+import SwiftUI
+
+struct SettingsHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsHeaderView()
+}
---
+++ MessageMe/ViewModel/Settings/SettingsCellViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsCellViewModel.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 8/7/2024.
+//
+
+import SwiftUI
+
+struct SettingsCellViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsCellViewModel()
+}
