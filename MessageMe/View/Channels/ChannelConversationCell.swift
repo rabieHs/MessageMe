e6aab07e4da125b7175e8b6d61cbbea5cfacac85
rabieHs <rabiehoussaini@gmail.com>
2024-07-14 17:32:13 +0100

    finish

---
+++ MessageMe/View/Channels/ChannelConversationCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelConversationCell.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 14/7/2024.
+//
+
+import SwiftUI
+
+struct ChannelConversationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChannelConversationCell()
+}
---
+++ MessageMe/ViewModel/Chat/ChannelViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChannelViewModel.swift
+//  MessageMe
+//
+//  Created by rabie houssaini on 14/7/2024.
+//
+
+import Foundation
